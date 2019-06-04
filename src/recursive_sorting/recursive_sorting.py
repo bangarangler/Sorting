c7defaa1a36bd7b437891030a9629a54c2fe1938
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-06-04 18:32:58 -0400

    Added in inplace Mergesort for reference

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,5 +1,4 @@
-# testArr = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-testArr = [0,9]
+testArr = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
@@ -37,15 +36,37 @@ def merge_sort( arr ):
 
 print(merge_sort(testArr))
 # STRETCH: implement an in-place merge sort algorithm
-def merge_in_place(arr, start, mid, end):
-    # TO-DO
+# def merge_in_place(arr, start, mid, end):
+    # # TO-DO
 
-    return arr
+    # return arr
 
-def merge_sort_in_place(arr, l, r):
-    # TO-DO
+# def merge_sort_in_place(arr, l, r):
+    # # TO-DO
 
-    return arr
+    # return arr
+
+# NOT MY CODE. JUST WANTED TO SEE HOW IT WAS DONE FOR REFERENCE GOING FORWARD
+import random
+
+def merge_sort(xs):
+    unit = 1
+    while unit <= len(xs):
+        h = 0
+        for h in range(0,len(xs),unit *2):
+            l,r = h, min(len(xs), h + 2 * unit)
+            mid = h + unit
+            p,q = l, mid
+            while p < mid and q < r:
+                if xs[p] < xs[q]: p += 1
+                else:
+                    tmp = xs[q]
+                    xs[p + 1: q + 1] = xs[p:q]
+                    xs[p] = tmp
+                    p, mid, q = p + 1, mid + 1, q + 1
+        unit *= 2
+    return xs
+print(merge_sort(testArr))
 
 
 # STRETCH: implement the Timsort function below
